7b3d83f9ba11c3bb3c7f2378385286a250b2f605
yitingko <147891948+yitingko@users.noreply.github.com>
2023-11-04 06:25:27 +0000

    1104after noon

---
+++ tools.py
@@ -0,0 +1,40 @@
+import random
+
+def playGame():
+    min = 1
+    max = 100
+    count = 0
+    target = random.randint(min,max)
+    print(target)
+    print("============猜數字遊戲=================\n")
+    while(True):
+        keyin = int(input(f"猜數字的範圍{min}~{max}:"))
+        count += 1
+        if(keyin >= min and keyin <= max):
+            if keyin == target:
+                print(f"賓果!猜對了, 答案是:{keyin}")
+                break
+            elif keyin > target:
+                print("再小一點")
+                max = keyin - 1
+            elif keyin < target:
+                print("再大一點")
+                min = keyin + 1
+            print(f"加油@您已經猜了{count}次")
+            continue
+        else:
+            print("請輸入提示範圍內的數字")
+    print(f"您猜了{count}次")
+
+class Student():
+    def __init__(self,name:str,chinese:int,english:int,math:int):
+        self.name = name
+        self.chinese = chinese
+        self.english = english
+        self.math = math
+
+    def sum(self)->int: #實體方法
+        return self.chinese + self.english + self.math
+    
+def getStudent(name:str,chinese:int,math:int,english:int) -> Student:
+    return Student(name=name,chinese=chinese,english=english,math=math)
